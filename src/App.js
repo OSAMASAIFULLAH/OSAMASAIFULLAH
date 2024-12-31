e7408ee3947c276da9e820aeaf6c860ef258303e
OSAMA SAIFULLAH <usamasaif4488@gmail.com>
2024-12-31 12:43:34 +0500

    calculator

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Clc from "./Components/Clc";
+function App(){
+  let n="simple calculator"
+
+  return(
+    <div>
+      <Clc name={n} />
+
+    </div>
+  )
+}
+export default App;
---
+++ src/Components/Clc.js
@@ -0,0 +1,176 @@
+import { useState } from "react"
+function Clc(props){
+    const [typedNumbers, setTypedNumbers] = useState(""); 
+
+    const handleclick = (digit) => {
+        setTypedNumbers((prev) => prev + digit);
+    };
+   return(<> <div style={{
+    display:"flex",
+    justifySelf:"center",
+
+   }}>
+        <h4 style={{marginRight:"700px",}}>{props.name}</h4>
+    </div>
+    <div style={{
+        width:"100vh",
+        height:"100vh",
+        display:"flex",
+        justifyContent:"center",
+    }}>
+        <div style={{
+            height: "508px",
+            width: "370px",
+            border:"2px solid black",
+            backgroundColor:' #cacaca',
+            display:"flex",
+            justifyContent:"center",
+            flexDirection:"column",
+            borderRadius:"3px",
+            // backgroundColor:"black",
+            
+        }}>
+            
+            <div style={{height:"150px",
+                width:"350px",
+                border:"2px solid black",
+                marginTop:"3px",
+                borderRadius:"10px",
+                marginLeft:"8px",
+                backgroundColor:"#7a6666",
+            }}>
+<h3 id="one"> {typedNumbers}</h3>
+            </div>
+            <div style={{height:"400px",
+                width:"350px",
+                border:"2px solid black",
+                marginTop:"3px",
+                borderRadius:"10px",
+                marginLeft:"8px",
+                marginBottom:"3px",
+            }}>
+                 <div style={{height:"50px",
+                width:"340px",
+                // border:"2px solid black",
+                marginTop:"3px",
+                // borderRadius:"10px",
+                // margin:"10px",
+                // marginBottom:"3px",
+                marginTop:"30px",
+                marginLeft:"3px",
+                display:"flex",
+                justifyContent:"space-around"
+
+            }}>
+         <button style={{
+            padding:"10px 40px",
+            fontSize:"30px",
+            
+         }} onClick={()=>{handleclick(1)}}>1</button>
+         <button style={{
+            padding:"10px 40px",
+            fontSize:"30px",
+         }} onClick={()=>{handleclick(2)}}>2</button>
+         <button style={{
+            padding:"10px 40px",
+            fontSize:"30px",
+         }}  onClick={()=>{handleclick(3)}}>3</button>
+         
+
+            </div>
+                 <div style={{height:"50px",
+                width:"340px",
+                // border:"2px solid black",
+                marginTop:"3px",
+                borderRadius:"10px",
+               
+                marginTop:"30px",
+                marginLeft:"3px",
+                marginTop:"30px",
+                marginLeft:"3px",
+                display:"flex",
+                justifyContent:"space-around"
+
+            }}>
+                  <button style={{
+            padding:"10px 40px",
+            fontSize:"30px",
+         }} onClick={()=>{handleclick(4)}}>4</button>
+         <button style={{
+            padding:"10px 40px",
+            fontSize:"30px",
+         }} onClick={()=>{handleclick(5)}}>5</button>
+         <button style={{
+            padding:"10px 40px",
+            fontSize:"30px",
+         }} onClick={()=>{handleclick(6)}}>6</button>
+
+            </div>
+                 <div style={{height:"50px",
+                width:"340px",
+                // border:"2px solid black",
+                marginTop:"3px",
+                borderRadius:"10px",
+                // margin:"10px",
+                // marginBottom:"3px",
+                marginTop:"30px",
+                marginLeft:"3px",
+                display:"flex",
+                justifyContent:"space-around"
+
+            }}>
+         
+         <button style={{
+            padding:"10px 40px",
+            fontSize:"30px",
+         }} onClick={()=>{handleclick(7)}}>7</button>
+         <button style={{
+            padding:"10px 40px",
+            fontSize:"30px",
+         }} onClick={()=>{handleclick(8)}}>8</button>
+         <button style={{
+            padding:"10px 40px",
+            fontSize:"30px",
+         }} onClick={()=>{handleclick(9)}}>9</button>
+            </div>
+                 <div style={{height:"50px",
+                width:"340px",
+                // border:"2px solid black",
+                marginTop:"3px",
+                borderRadius:"10px",
+                // margin:"10px",
+                // marginBottom:"3px",
+                marginTop:"30px",
+                marginLeft:"3px",
+                 display:"flex",
+                justifyContent:"space-around"
+
+            }}>
+         
+         <button style={{
+             padding:"7px 28px",
+            fontSize:"30px",
+         }} onClick={()=>{handleclick("+")}}>+</button>
+         <button style={{
+             padding:"7px 28px",
+            fontSize:"30px",
+         }}onClick={()=>{handleclick("-")}}>-</button>
+         <button style={{
+ padding:"7px 28px",
+             fontSize:"30px",
+         }}onClick={()=>{handleclick("*")}}>*</button>
+         <button style={{
+            padding:"7px 28px",
+            fontSize:"30px",
+         }}onClick={()=>{handleclick("/")}}>/</button>
+            </div>
+           
+
+            </div>
+
+        </div>
+    </div>
+    </>
+   )
+}
+export default Clc
